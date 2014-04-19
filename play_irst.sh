2772d5d91c020cb4a1a36c87b5c80896a8de6295
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2014-04-19 22:11:42 +0530

    scripts that return a single file to be played for the timezone

---
+++ play_irst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+array=($(find ~ -maxdepth 1 -name "*.mp3" -printf "%f\n" |   gawk -F"." '{ print $1 }'  | sort -g ))
+timezone=$(date --date="now + 209 min" +"%s")
+for index in ${!array[*]}
+do
+    if [ ${array[$index]} -gt $timezone ]
+        then
+        echo "/home/ubuntu/"${array[$index]}".mp3"
+        break
+    fi
+done
